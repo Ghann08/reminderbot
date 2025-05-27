cc8d37c60d15827a75d85a231c85bd16f5f0332f
jeka <eginminozenko@gmail.com>
2025-05-27 13:36:35 +0300

    создал BaseModel напоминия

--- services/remindres.py
+++ services/remindres.py
@@ -1,5 +1,21 @@
+import asyncio
 from datetime import datetime
+from uuid import uuid4, UUID
 
 class Reminders():
     text: str
     date: datetime
+    id_remind: UUID
+
+    def __init__(self, text: str, date: datetime):
+        self.text = text
+        self.id_remind = uuid4()
+        if self.text != None:
+            asyncio.get_event_loop().create_task(create_remind(self.text))
+        else:
+            print("okey")
+
+async def create_remind(text):
+    print("sdsds")
+    await asyncio.sleep(5)
+    print(text)
