d5efbd2fd93ff5edafa7afb1f95eb7f410fb21b8
jeka <eginminozenko@gmail.com>
2025-05-28 10:51:35 +0300

    создал BaseModel напоминия создание напоминия и функция update

--- services/remindres.py
+++ services/remindres.py
@@ -7,16 +7,32 @@ class Reminders():
     date: datetime
     id_remind: UUID
 
-    def __init__(self, text: str, date: datetime):
+    def __init__(self, text: str | None = None, date: datetime | None = None):
         self.text = text
+        self.date = date
         self.id_remind = uuid4()
-        if self.text != None:
-            asyncio.create_task(self.create_remind())
+        if self.text != None and self.date != None:
+            tm_diff = self.date - datetime.now()
+            asyncio.create_task(self.create_remind(tm_diff))
         else:
+            print(self.text)
+            print(self.date)
             print("okey")
 
-    async def create_remind(self):
+    def update(self, text = None , date = None):
+        if self.text != text and text != None:
+            self.text = text
+        if self.date != date and date != None:
+            self.date = date
+        if self.text != None and self.date != None:
+            tm_diff = self.date - datetime.now()
+            asyncio.create_task(self.create_remind(tm_diff))
+        else:
+            print(self.text)
+            print(self.date)
+            print("okey")
+
+    async def create_remind(self, tm_diff):
         print("Напоминание создано")
-        tm_diff = self.date = datetime.now()
-        await asyncio.sleep(5)
+        await asyncio.sleep(int(tm_diff.total_seconds()))
         print(f"Напоминание: {self.text}")
